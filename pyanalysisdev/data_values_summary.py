27b7150d6e07d7d8d359b82a2def4afafd10b3d3
richardangell <richardangell37@gmail.com>
2019-04-14 11:02:21 +0100

    add input checking to data_values_summary

--- pyanalysisdev/data_values_summary.py
+++ pyanalysisdev/data_values_summary.py
@@ -1,18 +1,63 @@
 import pandas as pd
 
 
+
 def data_values_summary(df, columns = None, max_values = 50, summary_values = 5, top_mid_bottom = 5):
-    
-    if columns == None:
+    '''Function to produce summaries of values in a DataFrame.'''
+
+    if not isinstance(df, pd.DataFrame):
+
+        raise TypeError('df should be a pandas DataFrame')
+
+    if columns is None:
         
         columns = df.columns.values
+
+    else:
+
+        if not isinstance(columns, list):
+
+            raise TypeError('columns should be a list (of columns in df)')
     
-    columns_summary = [summarise_column(df, col, max_values, summary_values, top_mid_bottom) for col in columns]
+        for col in columns:
+
+            if not col in df.columns.values:
+
+                raise ValueError(str(col) + ' not in df')
+
+    if not isinstance(max_values, int):
+
+        raise TypeError('max_values should be an int')
+
+    if not max_values > 0:
+
+        raise ValueError('max_values must be greater than 0')
+
+    if not isinstance(summary_values, int):
+
+        raise TypeError('summary_values should be an int')
+
+    if not summary_values > 0:
+
+        raise ValueError('summary_values must be greater than 0')
+
+    if not isinstance(top_mid_bottom, int):
+
+        raise TypeError('top_mid_bottom should be an int')
+
+    if not top_mid_bottom > 0:
+
+        raise ValueError('top_mid_bottom must be greater than 0')
+
+    columns_summary = [
+        summarise_column(df, col, max_values, summary_values, top_mid_bottom) for col in columns
+    ]
     
     columns_summary_all = pd.concat(columns_summary, axis = 1)
 
     return(columns_summary_all)
     
+
 
 def summarise_column(df, column, max_values, summary_values, top_mid_bottom):
     
@@ -24,6 +69,7 @@ def summarise_column(df, column, max_values, summary_values, top_mid_bottom):
     
     return(value_counts_resize)
     
+
     
 def resize_column_summary(df, max_values, summary_values, top_mid_bottom):
     
@@ -75,3 +121,7 @@ def resize_column_summary(df, max_values, summary_values, top_mid_bottom):
             select_rows = select_rows.append(append_rows)
     
         return(select_rows.reset_index(drop = True))
+
+
+
+        
