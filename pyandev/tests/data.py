8ae32e813a71643582c03beadc299215c59ec42c
richardangell <richardangell37@gmail.com>
2019-04-14 20:31:56 +0100

    add option to return random weights column on boston df

--- pyandev/tests/data.py
+++ pyandev/tests/data.py
@@ -4,7 +4,7 @@ from sklearn.datasets import load_boston
 
 
 
-def load_boston_df(insert_missings = False):
+def load_boston_df(insert_missings = False, add_weights_column = False):
     '''Load boston dataset from sklearn and prepare it as a pandas DataFrame.'''
 
     if not isinstance(insert_missings, bool):
@@ -24,6 +24,12 @@ def load_boston_df(insert_missings = False):
     boston_df = pd.DataFrame(boston['data'], columns = boston['feature_names'])
 
     boston_df['target'] = boston['target']
+
+    if add_weights_column:
+
+        np.random.seed(5)
+
+        boston_df['weights'] = np.random.randint(low = 0, high = 10, size = boston_df.shape[0]) / 3
 
     return boston_df
 
