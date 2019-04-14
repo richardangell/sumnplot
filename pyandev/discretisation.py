b5b373a5c6ebe39be6969a6515fa9ddd696a4d19
richardangell <richardangell37@gmail.com>
2019-04-14 19:26:47 +0100

    document and improve discretisation functions

--- pyandev/discretisation.py
+++ pyandev/discretisation.py
@@ -1,20 +1,72 @@
 import pandas as pd
 import numpy as np
+from pandas.api.types import is_numeric_dtype
 
 
-def discretise(data, bucketing_type, variable, n = None, quantiles = None, weights = None):
+def discretise(df, variable, n = None, bucketing_type = 'equal_width', weights_column = None, quantiles = np.linspace(0, 1, 11)):
+    '''Function to discretise a numeric variable with one of the following approaches; equal_width, 
+    equal_weight, quantile or weighted_quantile buckets.
+    
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Data of interest, must contain a column supplied in variable.
+        
+    variable : str
+        Column to bucket in df. Must be numeric type.
+
+    n : int
+        Number of buckets to use in case of "equal_width" or "equal_weight" bucketing.
+
+    bucketing_type : str, default = "equal_width"
+        One of the following "equal_width", "equal_weight", "quantile" or "weighted_quantile".
+        The type of bucketing to use.
+
+    weights_column : str, default = None
+        Column of weights in df. Default value of None results in equal weighted observations. Only used
+        for "equal_weight" or "weighted_quantile" buckets.
+
+    quantiles : float or array-like, default = np.linspace(0, 1, 11)
+        Value(s) between 0 <= quantiles <= 1, the quantile(s) to compute.
+
+    Returns
+    -------
+    bucketed_variable : pd.Series 
+        Categorical version (pandas category type) of df[variable] bucketed according to bucketing_type.
+        
+    '''
+
+    if not isinstance(df, pd.DataFrame):
+
+        raise TypeError('df must be a pandas DataFrame')
 
     type_valid_options = ['equal_width', 'equal_weight', 'quantile', 'weighted_quantile']
 
-    assert bucketing_type in type_valid_options, 'invalid bucketing_type; %s, valid; %s' % (type, type_valid_options)
+    if not isinstance(bucketing_type, str):
 
-    assert type(data) is pd.DataFrame, 'data is not a pandas DataFrame'
+        raise TypeError('bucketing_type must be a str')
 
-    assert variable in data.columns.values, 'variable; %s is not in data' % variable
+    if not bucketing_type in type_valid_options:
 
-    if weights is not None:
+        raise ValueError('invalid value for bucketing_type; ' +  bucketing_type)
 
-        assert weights in data.columns.values, 'weights; %s is not in data' % weights
+    if not isinstance(variable, str):
+
+        raise ValueError('variable must be a str')
+
+    if not variable in df.columns.values:
+        
+        raise ValueError('variable; ' + variable + ' not in df')
+
+    if weights_column is not None:
+
+        if not isinstance(weights_column, str):
+
+            raise TypeError('weights_column must be a str')
+
+        if not weights_column in df.columns.values:
+
+            raise ValueError('weights_column; ' + weights_column + ' not in df')
 
     if quantiles is not None:
 
@@ -22,46 +74,99 @@ def discretise(data, bucketing_type, variable, n = None, quantiles = None, weigh
 
         quantiles = np.unique(np.sort(np.append(quantiles, [0, 1])))
 
-        assert np.all(quantiles >= 0), 'quantiles should be all greater than or equal to 0'
+        if not np.all(quantiles >= 0):
 
-        assert np.all(quantiles <= 1), 'quantiles should be all less than or equal to 1'
+            raise ValueError('quantiles should be all greater than or equal to 0')
+
+        if not np.all(quantiles <= 1):
+
+            raise ValueError('quantiles should be all less than or equal to 1')
 
     if bucketing_type == 'equal_width':
 
-        bucketed_variable = equal_width(variable = data[variable],
-                                        n = n)
+        bucketed_variable = equal_width(
+            df = df, 
+            variable = variable, 
+            n = n,
+        )
 
     elif bucketing_type == 'equal_weight':
 
-        bucketed_variable = equal_weight(data = data, 
-                                         variable = variable,
-                                         weights = weights,
-                                         n = n)
+        bucketed_variable = equal_weight(
+            df = df, 
+            variable = variable,
+            weights = weights_column,
+            n = n,
+        )
 
     elif bucketing_type == 'quantile':
 
-        bucketed_variable = quantile(data = data, 
-                                     variable = variable,
-                                     quantiles = quantiles)
+        bucketed_variable = quantile(
+            df = df, 
+            variable = variable,
+            quantiles = quantiles,
+        )
 
     elif bucketing_type == 'weighted_quantile':
 
-        bucketed_variable = weighted_quantile(data = data, 
-                                              variable = variable,
-                                              weights = weights,
-                                              quantiles = quantiles)
+        bucketed_variable = weighted_quantile(
+            df = df, 
+            variable = variable,
+            weights = weights_column,
+            quantiles = quantiles,
+        )
 
-    else:
-
-        raise ValueError('unexpected value for bucketing_type %s' % bucketing_type)
-
-    return(bucketed_variable)
+    return bucketed_variable
 
 
-def equal_width(variable, n):
+
+def equal_width(df, variable, n):
+    '''Function to split variable into n buckets of equal width.
+    
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Data of interest, must contain a column supplied in variable.
+        
+    variable : str
+        Column to bucket in df. Must be numeric type.
+    
+    n : int
+        Number of buckets.
+
+    Returns
+    -------
+    cat : pd.Series 
+        Categorical version (pandas category type) of df[variable] bucketed into n
+        levels of equal width. 
+    '''
+
+    if not isinstance(df, pd.DataFrame):
+
+        raise TypeError('df should be a pd.DataFrame')
+
+    if not isinstance(variable, str):
+
+        raise TypeError('variable must be a str')
+
+    if not variable in df.columns.values:
+
+        raise ValueError('variable; ' + variable + ' not in df')
+
+    if not is_numeric_dtype(df[variable]):
+
+        raise TypeError('df[' + variable + '] is not numeric type')
+
+    if not isinstance(n, int):
+
+        raise TypeError('n must be an int')
+
+    if not n > 0:
+
+        raise ValueError('n must be greater than 0')
 
     # see https://github.com/pandas-dev/pandas/issues/17047 for issues with include_lowest
-    variable_cut = pd.cut(variable, n, include_lowest = True)
+    variable_cut = pd.cut(df[variable], n, include_lowest = True, duplicates = 'drop')
 
     variable_cut = add_null_category(variable_cut)
 
@@ -69,124 +174,314 @@ def equal_width(variable, n):
 
 
 
-def equal_weight(data, variable, weights, n):
+def equal_weight(df, variable, n, weights = None):
+    '''Function to split variable into n buckets of equal weight.
+    
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Data of interest, must contain a column supplied in variable.
+        
+    variable : str
+        Column to bucket in df. Must be numeric type.
 
-    if weights == None:
+    weights : str
+        Column of weights in df. Default value of None results in equal weighted observations.
 
-        df = data[[variable]].copy()
+    n : int
+        Number of buckets.
 
-        df ['weights'] = 1
+    Returns
+    -------
+    cat : pd.Series 
+        Categorical version (pandas category type) of df[variable] bucketed into n
+        levels of equal weight. 
+    '''
 
-        weights = 'weights'
+    if not isinstance(df, pd.DataFrame):
 
-        df.sort_values(variable, inplace = True)
+        raise TypeError('df should be a pd.DataFrame')
+
+    if not isinstance(variable, str):
+
+        raise TypeError('variable must be a str')
+
+    if not variable in df.columns.values:
+
+        raise ValueError('variable; ' + variable + ' not in df')
+
+    if not is_numeric_dtype(df[variable]):
+
+        raise TypeError('df[' + variable + '] is not numeric type')
+
+    if not isinstance(n, int):
+
+        raise TypeError('n must be an int')
+
+    if not n > 0:
+
+        raise ValueError('n must be greater than 0')
+
+    if weights is None:
+
+        weights = np.ones(df.shape[0])
 
     else:
 
-        df = data[[variable, weights]].sort_values(variable)
+        if not isinstance(weights, str):
 
-    # group by variable of interest to get weights and count by each data value
-    summary = df.groupby(df[variable]).agg({weights: ['sum', 'count']})
+            raise TypeError('weights must be a str')
 
-    summary.columns = summary.columns.get_level_values(1)
+        if not weights in df.columns.values:
 
-    summary.reset_index(inplace = True)
+            raise ValueError('weights; ' + weights + ' not in df')
 
-    bin_weight = summary['sum'].sum() / n
+        if not is_numeric_dtype(df[weights]):
 
-    rows = []
-    weighted_cut_points = []
+            raise TypeError('df[' + weights + '] is not numeric type')
 
-    bucket_sum = 0
+        weights = df[weights]
 
-    for i in range(summary.shape[0]):
-        
-        bucket_sum += summary.iloc[i]['sum']
-        
-        if bucket_sum >= bin_weight:
-            
-            rows.append(i)
-            
-            weighted_cut_points.append(summary.iloc[i][variable])
-            
-            bucket_sum = 0
-    
-    # if the last value that was added was not from the last row
-    if (weighted_cut_points[len(weighted_cut_points)-1]) != summary.iloc[i][variable]:
-        
-        rows.append(i)
-        
-        weighted_cut_points.append(summary.iloc[i][variable])
+    weight_quantiles = compute_weighted_quantile(
+        values = df[variable], 
+        quantiles = np.array(np.linspace(start = 0, stop = 1, num = n + 1)), 
+        sample_weight = weights, 
+        values_sorted = False,
+    )
 
-    variable_cut = pd.cut(data[variable], weighted_cut_points, include_lowest = True)
+    variable_cut = pd.cut(df[variable], np.unique(weight_quantiles), include_lowest = True, duplicates = 'drop')
     
     variable_cut = add_null_category(variable_cut)
 
-    return(variable_cut)
+    return variable_cut
 
 
 
+def quantile(df, variable, quantiles):
+    '''Function to split variable into buckets according to the supplied quantiles.
+    
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Data of interest, must contain a column supplied in variable.
+        
+    variable : str
+        Column to bucket in df. Must be numeric type.
+    
+    quantiles : float or array-like
+        Value(s) between 0 <= quantiles <= 1, the quantile(s) to compute.
 
-def quantile(data, variable, quantiles):
+    Returns
+    -------
+    cat : pd.Series 
+        Categorical version (pandas category type) of df[variable] bucketed according to
+        the supplied quantiles. 
+    '''
 
-    quantile_values = data[variable].quantile(quantiles)
+    if not isinstance(df, pd.DataFrame):
 
-    variable_cut = pd.cut(data[variable], np.unique(quantile_values), include_lowest = True)
+        raise TypeError('df should be a pd.DataFrame')
+
+    if not isinstance(variable, str):
+
+        raise TypeError('variable must be a str')
+
+    if not variable in df.columns.values:
+
+        raise ValueError('variable; ' + variable + ' not in df')
+
+    if not is_numeric_dtype(df[variable]):
+
+        raise TypeError('df[' + variable + '] is not numeric type')
+
+    if not np.all(quantiles >= 0):
+
+        raise ValueError('quantiles should be all greater than or equal to 0')
+
+    if not np.all(quantiles <= 1):
+
+        raise ValueError('quantiles should be all less than or equal to 1')
+
+    quantile_values = df[variable].quantile(quantiles)
+
+    variable_cut = pd.cut(df[variable], np.unique(quantile_values), include_lowest = True, duplicates = 'drop')
 
     variable_cut = add_null_category(variable_cut)
 
-    return(variable_cut)    
+    return variable_cut    
 
 
 
+def compute_weighted_quantile(values, quantiles, sample_weight = None, values_sorted = False):
+    '''Funtion to calculate weighted percentiles.
 
-def weighted_quantile(data, variable, weights, quantiles):
+    Code modified from the answer given by users Alleo & Max Ghenis on stackoverflow; https://stackoverflow.com/a/29677616.
+    See https://en.wikipedia.org/wiki/Percentile#The_weighted_percentile_method for description of method.
+    Removed the old_style arg and associated code from stackoverflow answer.
 
-    if weights == None:
-
-        df = data.loc[~data[variable].isnull(), [variable]].copy()
-
-        df['weights'] = 1
-
-        weights = 'weights'
-
-        df.sort_values(variable, inplace = True)
-
-    else:
-
-        df = data.loc[~data[variable].isnull(), [variable, weights]].sort_values(variable)
-
-    df['cumsum_weights'] = df[weights].cumsum() / df[weights].sum()
-
-    j = 0
-
-    weighted_cut_points = []
-
-    for i in range(df.shape[0]):
+    Parameters
+    ----------
+    values : array-like
+        Data of interest, must contain a column supplied in variable.
         
-        if df.iloc[i]['cumsum_weights'] >= quantiles[j]:
+    quantiles : array-like
+        Value(s) between 0 <= quantiles <= 1, the weighted quantile(s) to compute.
+    
+    sample_weight : array-like, default = None
+        Array of weights, must be same length as values. Default value of None 
+        means each observation in values is equally weighted.
 
-            weighted_cut_points.append(df.iloc[i][variable])
+    values_sorted : bool
+        Are the values and sample_weight arrays pre-sorted? If True arrays will not
+        be sorted in function.
 
-            j += 1
+    Returns
+    -------
+    cat : np.array 
+        Computed (weighted) quantiles. 
+    '''
 
-    variable_cut = pd.cut(data[variable], np.unique(weighted_cut_points), include_lowest = True)
+    if not np.all(quantiles >= 0) and np.all(quantiles <= 1):
+
+        raise ValueError('quantiles must be in the range [0, 1]')
+
+    values = np.array(values)
+    quantiles = np.array(quantiles)
+    quantiles = np.unique(np.sort(np.append(quantiles, [0, 1])))
+    if sample_weight is None:
+        sample_weight = np.ones(len(values))
+    sample_weight = np.array(sample_weight)
+
+    if not values_sorted:
+        sorter = np.argsort(values)
+        values = values[sorter]
+        sample_weight = sample_weight[sorter]
+
+    weighted_quantiles = np.cumsum(sample_weight) - 0.5 * sample_weight
+
+    weighted_quantiles /= np.sum(sample_weight)
+
+    return np.interp(quantiles, weighted_quantiles, values)
+
+
+
+def weighted_quantile(df, variable, quantiles, weights = None):
+    '''Function to split variable into buckets according to the supplied weighted quantiles.
+    
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Data of interest, must contain a column supplied in variable.
+        
+    variable : str
+        Column to bucket in df. Must be numeric type.
+    
+    quantiles : float or array-like
+        Value(s) between 0 <= quantiles <= 1, the quantile(s) to compute.
+
+    weights : str, default = None
+        Column of weights in df. Default value of None results in observations treated as equally weighted.
+
+    Returns
+    -------
+    cat : pd.Series 
+        Categorical version (pandas category type) of df[variable] bucketed according to
+        the supplied quantiles. 
+    '''
+
+    if not isinstance(df, pd.DataFrame):
+
+        raise TypeError('df should be a pd.DataFrame')
+
+    if not isinstance(variable, str):
+
+        raise TypeError('variable must be a str')
+
+    if not variable in df.columns.values:
+
+        raise ValueError('variable; ' + variable + ' not in df')
+
+    if not is_numeric_dtype(df[variable]):
+
+        raise TypeError('df[' + variable + '] is not numeric type')
+
+    if not np.all(quantiles >= 0):
+
+        raise ValueError('quantiles should be all greater than or equal to 0')
+
+    if not np.all(quantiles <= 1):
+
+        raise ValueError('quantiles should be all less than or equal to 1')
+
+    if not weights is None:
+
+        if not isinstance(weights, str):
+
+            raise TypeError('weights must be a str')
+
+        if not weights in df.columns.values:
+
+            raise ValueError('weights; ' + weights + ' not in df')
+
+        if not is_numeric_dtype(df[weights]):
+
+            raise TypeError('df[' + weights + '] is not numeric type')
+
+        weights = df[weights]
+
+    weight_quantiles = compute_weighted_quantile(
+        values = df[variable], 
+        quantiles = quantiles, 
+        sample_weight = weights, 
+        values_sorted = False,
+    )
+
+    variable_cut = pd.cut(df[variable], np.unique(weight_quantiles), include_lowest = True, duplicates = 'drop')
     
     variable_cut = add_null_category(variable_cut)
 
-    return(variable_cut)
+    return variable_cut
 
 
 
+def add_null_category(categorical_variable, null_category_name = 'Null'):
+    '''Function to add new categorical level to categorical variable and set NAs to this category.
+    
+    Parameters
+    ----------
+    categorical_variable : pd.Series
+        Categorical variable to add null categorical level to.
+        
+    null_category_name : str, default = 'Null' 
+        The name of the categorical level for null values to add.
 
+    Returns
+    -------
+    cat : pd.Series 
+        Categorical variable (pandas category type) with null categorical level added.
+    '''
 
-def add_null_category(categorical_variable):
+    if not isinstance(categorical_variable, pd.Series):
 
-    cat = categorical_variable.cat.add_categories(['Null'])
+        raise TypeError('categorical_variable should be a pandas Series')
+
+    if not categorical_variable.dtype.name == 'category':
+
+        raise TypeError('categorical_variable should be category dtype')
+
+    if not isinstance(null_category_name, str):
+
+        raise TypeError('null_category_name must be a str')
+
+    if null_category_name in categorical_variable.cat.categories:
+
+        raise ValueError('null_category_name; ' + null_category_name + ' already in categorical_variable.cat.categories')
+
+    cat = categorical_variable.cat.add_categories([null_category_name])
 
     cat.fillna('Null', inplace = True)
 
-    return(cat)
+    return cat
 
 
 
