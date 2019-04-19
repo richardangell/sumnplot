ac847a67af65cbb4fda0cf874129282ee079b373
richardangell <richardangell37@gmail.com>
2019-04-19 19:32:30 +0100

    add documentation and more input checking to tests.data.load_boston_df()

--- pyandev/tests/data.py
+++ pyandev/tests/data.py
@@ -4,12 +4,47 @@ from sklearn.datasets import load_boston
 
 
 
-def load_boston_df(insert_missings = False, add_weights_column = False, add_dummy_predictions = False, add_dummy_predictions2 = False):
-    '''Load boston dataset from sklearn and prepare it as a pandas DataFrame.'''
+def load_boston_df(insert_missings = False, 
+                   add_weights_column = False, 
+                   add_dummy_predictions = False, 
+                   add_dummy_predictions2 = False):
+    '''Load boston dataset from sklearn and prepare it as a pandas DataFrame.
+    
+    Parameters
+    ----------
+    insert_missings : bool, default = False
+        Should nulls be inserted into the data? If True a random 10% of records (different across columns)
+        are set to null.
+        
+    add_weights_column : bool, default = False
+        Should a weights column be added to the data? If True random weights are added to each row. 
+        Weights take values of 0 to 10/3, in steps of 1/3.
+
+    add_dummy_predictions : bool, default = False
+        Should a column of dummy predictions be added to the data? If True normal(0, 3) noise is added to
+        the response as predictions.
+
+    add_dummy_predictions2 : bool, default = False
+        Should a column of dummy predictions be added to the data? If True normal(2, 3) noise is added to
+        the response as predictions.        
+
+    '''
 
     if not isinstance(insert_missings, bool):
 
         raise TypeError('insert_missings must be bool')
+
+    if not isinstance(add_weights_column, bool):
+
+        raise TypeError('add_weights_column must be bool')
+
+    if not isinstance(add_dummy_predictions, bool):
+
+        raise TypeError('add_dummy_predictions must be bool')
+
+    if not isinstance(add_dummy_predictions2, bool):
+
+        raise TypeError('add_dummy_predictions2 must be bool')
 
     boston = load_boston()
 
