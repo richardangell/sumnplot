1ff129c47c97e121f7bff0b48f7514cf8d82a9e4
richardangell <richardangell37@gmail.com>
2022-05-21 11:52:11 +0100

    create improved checks module

---
+++ src/sumnplot/checks.py
@@ -0,0 +1,42 @@
+"""Module for simple reusable checks."""
+
+import pandas as pd
+
+from typing import Any, Type, List
+
+
+def check_type(obj: Any, expected_types: List[Type], obj_name: str) -> None:
+    """Function to check object is of given types and raise a TypeError
+    if not.
+    """
+
+    if type(expected_types) is not list:
+
+        raise TypeError("expected_types must be a list")
+
+    if not all([type(expected_type) is type for expected_type in expected_types]):
+
+        raise TypeError("all elements in expected_types must be types")
+
+    if type(obj) not in expected_types:
+
+        raise TypeError(
+            f"{obj_name} is not in expected types {expected_types}, got {type(obj)}"
+        )
+
+
+def check_columns_in_df(df: pd.DataFrame, columns: List) -> None:
+    """Function to check that all specified columns are in a given DataFrame."""
+
+    check_type(df, [pd.DataFrame], "df")
+    check_type(columns, [list], "columns")
+
+    missing_columns = [
+        column_name for column_name in columns if column_name not in df.columns.values
+    ]
+
+    if len(missing_columns) > 0:
+
+        raise ValueError(
+            f"the following columns are missing from df; {missing_columns}"
+        )
