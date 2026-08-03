629e03e6457e6b2ef892993737a52288175f81a9
Richard Angell <richardangell37@gmail.com>
2026-08-03 17:47:24 +0100

    take version number from pyproject.toml

--- src/sumnplot/__init__.py
+++ src/sumnplot/__init__.py
@@ -1,4 +1,15 @@
 """Super simple summarisation and plotting."""
 
+import importlib.metadata
+
 from . import checks, discretisation, plot, summary
-from ._version import __version__
+
+__version__ = importlib.metadata.version("sumnplot")
+
+__all__ = [
+    "__version__",
+    "checks",
+    "discretisation",
+    "plot",
+    "summary",
+]
