9d35c7c39a7b4323214736b5ac14c48e0f6ec3b7
Richard Angell <richardangell37@gmail.com>
2026-08-06 19:29:46 +0100

    update imports

--- src/sumnplot/summarisation/__init__.py
+++ src/sumnplot/summarisation/__init__.py
@@ -3,13 +3,19 @@
 from sumnplot.summarisation.fill_out_summary_table import (
     ensure_all_level_combinations_populated,
 )
+from sumnplot.summarisation.group_by_weighted_average import (
+    ResponseWeight,
+    group_by_weighted_average,
+)
 from sumnplot.summarisation.weighted_average import (
     get_weighted_average_expr,
     get_weighted_average_expressions,
 )
 
 __all__ = [
+    "ResponseWeight",
     "ensure_all_level_combinations_populated",
     "get_weighted_average_expr",
     "get_weighted_average_expressions",
+    "group_by_weighted_average",
 ]
