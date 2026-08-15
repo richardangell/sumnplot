dbaa0d973df040a73f2348dccb57af30b7803589
Richard Angell <richardangell37@gmail.com>
2026-08-15 11:52:21 +0100

    add SummaryTable class

---
+++ tests/unit/summarisation/test_summary_table.py
@@ -0,0 +1,183 @@
+"""Unit tests for the summary_table module."""
+
+import re
+
+import polars as pl
+import pytest
+from polars.testing import assert_frame_equal
+
+from sumnplot.exceptions import MissingColumnError
+from sumnplot.summarisation.summary_operation import SummaryOperation
+from sumnplot.summarisation.summary_table import (
+    SummaryTable,
+    SummaryTableModificationError,
+    SummyTableError,
+)
+
+
+@pytest.fixture
+def sample_data() -> pl.DataFrame:
+    """Return a sample DataFrame for testing."""
+    return pl.DataFrame(
+        {
+            "group": ["A", "B", "C"],
+            "value": [10, 20, 30],
+        },
+    )
+
+
+@pytest.fixture
+def summary_table(sample_data: pl.DataFrame) -> SummaryTable:
+    """Return a SummaryTable with single group column and summarised value column."""
+    groupby_columns = ["group"]
+    summarised_column_types = {"value": SummaryOperation.SUM}
+    return SummaryTable(
+        sample_data,
+        groupby_columns=groupby_columns,
+        summarised_column_types=summarised_column_types,
+    )
+
+
+def test_valid_initialisation(summary_table: SummaryTable) -> None:
+    """Test that a SummaryTable instance can be created with valid inputs."""
+    assert summary_table is not None
+    assert summary_table.groupby_columns == ["group"]
+    assert summary_table.summarised_column_types == {"value": SummaryOperation.SUM}
+
+    expected_data = pl.DataFrame(
+        {
+            "group": ["A", "B", "C"],
+            "value": [10, 20, 30],
+        },
+    )
+
+    assert_frame_equal(
+        summary_table.head(5),
+        expected_data,
+    )
+
+    assert_frame_equal(
+        summary_table.tail(5),
+        expected_data,
+    )
+
+
+def test_empty_groupby_columns_raises_error(sample_data: pl.DataFrame) -> None:
+    """Test that an error is raised when groupby_columns is empty."""
+    with pytest.raises(
+        SummyTableError,
+        match=re.escape("Groupby columns must not be empty."),
+    ):
+        SummaryTable(
+            sample_data,
+            groupby_columns=[],
+            summarised_column_types={"value": SummaryOperation.SUM},
+        )
+
+
+def test_empty_summarised_column_types_raises_error(sample_data: pl.DataFrame) -> None:
+    """Test that an error is raised when summarised_column_types is empty."""
+    with pytest.raises(
+        SummyTableError,
+        match=re.escape("Summarised column types must not be empty."),
+    ):
+        SummaryTable(sample_data, groupby_columns=["group"], summarised_column_types={})
+
+
+def test_groupby_columns_not_unique_raises_error(sample_data: pl.DataFrame) -> None:
+    """Test that an error is raised when groupby_columns are not unique."""
+    with pytest.raises(
+        SummyTableError,
+        match=re.escape("Groupby columns must be unique."),
+    ):
+        SummaryTable(
+            sample_data,
+            groupby_columns=["group", "group"],
+            summarised_column_types={"value": SummaryOperation.SUM},
+        )
+
+
+def test_groupby_and_summarised_columns_overlap_raises_error(
+    sample_data: pl.DataFrame,
+) -> None:
+    """Test if groupby_columns and summarised_column_types overlap error is raised."""
+    with pytest.raises(
+        SummyTableError,
+        match=re.escape(
+            "Groupby columns and summarised columns must not overlap. "
+            "Overlapping columns: group.",
+        ),
+    ):
+        SummaryTable(
+            sample_data,
+            groupby_columns=["group"],
+            summarised_column_types={"group": SummaryOperation.SUM},
+        )
+
+
+def test_missing_columns_raise_exception_group(sample_data: pl.DataFrame) -> None:
+    """Test that an ExceptionGroup is raised when columns are missing from data."""
+    with pytest.raises(
+        ExceptionGroup,
+        match=re.escape("Missing columns in SummaryTable input data."),
+    ) as exc_info:
+        SummaryTable(
+            sample_data,
+            groupby_columns=["missing_group"],
+            summarised_column_types={"missing_value": SummaryOperation.SUM},
+        )
+
+    assert len(exc_info.value.exceptions) == 2
+
+    expected_missing_columns = [
+        "missing_group",
+        "missing_value",
+    ]
+
+    for raised_exception, expected_column in zip(
+        exc_info.value.exceptions,
+        expected_missing_columns,
+        strict=True,
+    ):
+        assert isinstance(raised_exception, MissingColumnError)
+        assert raised_exception.column == expected_column
+        assert (
+            raised_exception.message
+            == f"Column '{expected_column}' not found in DataFrame."
+        )
+
+
+def test_modifying_groupby_columns_raises_error(summary_table: SummaryTable) -> None:
+    """Test that modifying groupby_columns after initialisation raises an error."""
+    with pytest.raises(
+        SummaryTableModificationError,
+        match=re.escape("Cannot modify 'groupby_columns' on SummaryTable instance."),
+    ):
+        summary_table.groupby_columns = ["new_group"]
+
+    with pytest.raises(
+        SummaryTableModificationError,
+        match=re.escape("Cannot delete 'groupby_columns' on SummaryTable instance."),
+    ):
+        del summary_table.groupby_columns
+
+
+def test_modifying_summarised_column_types_raises_error(
+    summary_table: SummaryTable,
+) -> None:
+    """Test that modifying summarised_column_types raises an error."""
+    with pytest.raises(
+        SummaryTableModificationError,
+        match=re.escape(
+            "Cannot modify 'summarised_column_types' on SummaryTable instance.",
+        ),
+    ):
+        summary_table.summarised_column_types = {"new_value": SummaryOperation.SUM}
+
+    with pytest.raises(
+        SummaryTableModificationError,
+        match=re.escape(
+            "Cannot delete 'summarised_column_types' on SummaryTable instance.",
+        ),
+    ):
+        del summary_table.summarised_column_types
